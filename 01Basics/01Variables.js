01a4e421bc3a9ba0de12cfd3a96fee69536c037a
vasimgithub05 <106730921+vasimgithub05@users.noreply.github.com>
2023-12-17 14:21:47 +0000

    Variables

---
+++ 01Basics/01Variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553;
+let accountName = "Vasim";
+var accountEmail = "vasim@google.com";
+accountCity = "pune";
+let accountState;
+//accountId = 26 
+
+console.log(accountCity);
+
+console.table([accountId, accountName, accountEmail, accountCity, accountState])
+
