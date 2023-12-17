39dc65ae5460e7b43dfe1571008f991ef1938fef
vasimgithub05 <106730921+vasimgithub05@users.noreply.github.com>
2023-12-17 15:30:56 +0000

    ConversionOperators

---
+++ 01Basics/03ConversionOprators.js
@@ -0,0 +1,35 @@
+let score = "33abc"
+
+//console.log(typeof (score));
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLogedIn = 1
+let booleanIsLoggedIn = Boolean(isLogedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false;
+
+// "" => flase
+// "vasim" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+//console.log(typeof stringNumber)
+
+// 33 => string
+
+
+//*******************Operation********************** */
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
