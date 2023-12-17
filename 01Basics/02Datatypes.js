7201575e0f10eea7a7747deb72bb97373559705b
vasimgithub05 <106730921+vasimgithub05@users.noreply.github.com>
2023-12-17 14:21:13 +0000

    Datatypes

---
+++ 01Basics/02Datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all js code as a newar version
+//alert("Vasim") We are using node js not browser
+let name = "vasim"
+let age = 34
+let logedIn = true
+let pass;
+
+
+// Data Types
+// number => 2
+// bigint =>  large numer like trade app
+// string => "vasim"
+// boolean => true/false
+// null => Standalone value
+// undefined => 
+// symball => uniqe
+
+// Object
+
+console.log(typeof pass); //undefined
+console.log(typeof age); // Number
+console.log(typeof null); // object
+
