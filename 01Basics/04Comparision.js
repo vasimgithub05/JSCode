688049045cb094f32ed27f9da1dcd5c5d96f07e2
vasimgithub05 <106730921+vasimgithub05@users.noreply.github.com>
2023-12-17 15:49:03 +0000

    Comparision

---
+++ 01Basics/04Comparision.js
@@ -0,0 +1,21 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 1);
+console.log(null == 1);
+console.log(null != 1);
+
+console.log(undefined > 1);
+console.log(undefined == 1);
+console.log(undefined != 1);
+
+
+// === here forcefully check tyfof
+
+console.log("2" === 2);
