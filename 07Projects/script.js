68f9a5945dd9f1e7ef6441cfd9be63241ec93d00
vasimgithub05 <106730921+vasimgithub05@users.noreply.github.com>
2023-12-28 09:04:54 +0000

    test

---
+++ 07Projects/script.js
@@ -0,0 +1,32 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector("body") 
+const h1 = document.querySelector('h1')
+const h2 = document.querySelector('h2')
+
+buttons.forEach(function(button){
+    //console.log(button);
+    button.addEventListener('click', function(e){
+        // console.log(e);
+        // console.log(e.target);
+        if(e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id;
+            h1.style.color = 'white';
+            h2.style.color = 'white'
+        }
+        if(e.target.id === 'white'){
+            body.style.backgroundColor = e.target.id;
+            h1.style.color = 'black';
+            h2.style.color = 'black'
+        }
+       if(e.target.id === 'blue'){
+        body.style.backgroundColor = e.target.id;
+        h1.style.color = 'white';
+        h2.style.color = 'white'
+       }
+       if(e.target.id === 'yellow'){
+        body.style.backgroundColor = e.target.id;
+        h1.style.color = 'black';
+        h2.style.color = 'black'
+       }
+    })
+})
